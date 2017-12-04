cc6a83d43e0755c5b2d2a3a4b3316eed72986a99
sbhadsavle <sarang.bhadsavle@gmail.com>
2017-12-03 21:29:21 -0600

    Created new launch without turtlesim

--- nodes/gaze_speech_listener.py
+++ nodes/gaze_speech_listener.py
@@ -2,12 +2,31 @@
 import rospy
 from std_msgs.msg import String
 
+recent_key = 'none'
+
 def gaze_callback(data):
     print("Received:   " + str(data.data) + "\r")
 
 def speech_callback(data):
-    print("Received:   " + str(data.data) + "\r")
-    
+    #print("Received:   " + str(data.data) + "\r")
+    global recent_key
+    if data.data == '1':
+        recent_key = 'object'
+        #speechPub.publish("object")
+    elif data.data == '2':
+        recent_key = 'other'
+        #speechPub.publish("other")
+    elif data.data == '3':
+        recent_key = 'uhum'
+        #speechPub.publish("uhum")
+    elif data.data == '4':
+        recent_key = 'none'
+        #speechPub.publish("none")
+    else:
+        recent_key = 'none'
+        #speechPub.publish("none")
+
+   
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -18,10 +37,17 @@ def listener():
     rospy.init_node('gaze_speech_listener', anonymous=True)
 
     rospy.Subscriber("gaze", String, gaze_callback)
-    rospy.Subscriber("speech", String, speech_callback)
+    rospy.Subscriber("key", String, speech_callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    #gazePub = rospy.Publisher('gaze', String, queue_size=50)
+    speechPub = rospy.Publisher('speech', String, queue_size=50)
+
+    rate = rospy.Rate(1.0)
+    while not rospy.is_shutdown():
+        #gazePub.publish(rotationStr)
+        speechPub.publish(recent_key)
+        print("Publishing:   " + str(recent_key) + "\r")
+        rate.sleep()
 
 if __name__ == '__main__':
     listener()
