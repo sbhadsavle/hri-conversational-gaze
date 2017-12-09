fdc47304a7fb867aed8d677a20e8a8cc98f115a3
Cassidy <cassburden@gmail.com>
2017-12-08 21:54:34 -0600

    Conversations

---
+++ nodes/conversation_list.py
@@ -0,0 +1,128 @@
+conversations = [
+  [
+    # CHEEZ IT
+    [
+      "Hello, my name is Paul e, and this is my kitchen.",
+      "I really enjoy asking people about the items in my kitchen.",
+      "Let's go ahead and talking about some of the items now.",
+
+      "I really like Cheez It and I always eat some before going to sleep.",
+      "It's funny that I've never seen a box of Cheez Its before.",
+      "Could you please describe the Cheez It box to me?",
+    ],
+
+    [
+      "Hmm, I think the box doesn't do justice to Cheez Its.",
+      "Do you have any ideas to make the box more attractive?",
+      "I'm thinking of trying out some Pringles. Could you list 5 similarities between the box of Cheez It and Pringles can.",
+    ],
+    [
+      "I sometimes confuse Cheez Its with Pringles.",
+      "Can you tell me 5 differences between the box of Cheez It and Pringles can so it doesn't happen again?"
+    ],
+
+
+    # MUSTARD
+    [
+      "Thanks for your help, I really appreciate it.",
+      "Do you like mustard? I love mustard and always put it on my hotdogs.",
+      "I think the mustard bottle is really interesting also.",
+      "What do you think are the 5 most striking things about the bottle of mustard?"
+    ],
+    [
+      "What are 3 similarities between the mustard bottle and the jam bottle?"
+    ],
+
+    # BOX
+    [
+      "Could you describe the six pictures on the box?"
+    ],
+    [
+      "Which one is your favorite picture and why?"
+    ],
+
+    # GOODBYE
+    [
+      "Thanks so much for your help today. I really enjoyed talking with you."
+      "Goodbye."
+    ]
+  ],
+
+  [
+    # JAM
+    [
+      "Hello, my name is Paul e, and this is my kitchen.",
+      "I really enjoy asking people about the items in my kitchen.",
+      "Let's go ahead and talking about some of the items now.",
+ 
+      "I like peanut butter and jelly sandwiches. I especially like them with strawberry jam.",
+      "I'd like to find that same jam bottle the next time I go to the grocery.",
+      "Can you describe to me the jam bottle in detail so I find it at the grocery?",
+    ],
+    [
+      "The jam and mustard both come in such similar bottles that I have sometimes put mustard instead of jam on my peanut butter and jelly sandwich!",
+      "Can you give me 5 differences between the two bottles, so you can help me remember them correctly?"
+    ],
+
+    # PRINGLES
+    [
+      "Wow thank you, that's really helpful.",
+      "Oh that reminds me, see that Pringles can?"
+      "I have been eating the original flavour of Pringles all my life, so now I am bored of it."
+      "Do you happen to know any other flavours of chips that you would recommend me to try? "
+    ],
+    [
+      "I am serving Pringles at a party that I am throwing this weekend.",
+      "However, I have no clue what else to serve! Can you tell me what else can I have as finger food at this party?"
+    ],
+
+    # SUGAR
+    [
+      "Awesome! Thanks, you should stop by my party if you're free later.",
+      "Speaking of my party, I used the domino sugar when I was baking a birth day cake the other day.",
+      "I love cake, what about you? What type of sugary sweets do you like?",
+    ],
+    [
+      "Can you describe to me the differences between the Domino sugar box and the Cheez-Its box?"
+      "I wouldn't want to accidently add cheese crackers to my chocolate cake! Ha ha ha"
+    ],
+
+    # GOODBYE
+    [
+      "Thanks for all your help today. It's been really fun talking with you!",
+      "I am formally inviting you to my party! See you there!",
+      "Goodbye"
+    ]
+  ],
+
+
+  [
+    # PITCHER
+    [
+      "Hello, my name is Paul e, and this is my kitchen.",
+      "I really enjoy asking people about the items in my kitchen.",
+      "Let's go ahead and talking about some of the items now.",
+
+      "Can you describe the pitcher to me? What do you normally use it for?"
+    ],
+    [
+      "I remember when I was young, my robot mom used to serve lemonade in it.",
+      "Can you tell me how to make lemonade?"
+    ],
+
+    # TOASTEDS
+    [
+      "The box of toasteds has a sesame spinach spread recipe written on the back of it.",
+      "Can you tell me the directions of that recipe?",
+    ],
+    [
+      "I always get the box of toasteds confused with the box of Cheez Its!",
+      "Can you tell me 5 differences and 5 similarities between the two boxes?"
+    ],
+
+    [
+      "I use the wooden box to store my robot grandma's recipes. However, I think the box can be put to better use. What else do you think I can store in this wooden box?",
+      "I bought the box when I went to visit my robot grandma in California. Can you tell me a little bit about a trip you took recently?"
+    ]
+  ],
+]
